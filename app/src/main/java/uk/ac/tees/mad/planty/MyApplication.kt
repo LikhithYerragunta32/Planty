82d7f3b0672d6ce38f18e45f49d27fb162237521
Likhith Yerragunta <Likhith3008@gmail.com>
2025-10-28 12:00:00 +0530

    Configured the project with Gradle, establishing the foundational structure and necessary dependencies for the application

---
+++ app/src/main/java/uk/ac/tees/mad/planty/MyApplication.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.mad.planty
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+
+@HiltAndroidApp
+class MyApplication: Application() {
+
+
+}
---
+++ app/src/main/java/uk/ac/tees/mad/planty/di/HiltModule.kt
@@ -0,0 +1,10 @@
+package uk.ac.tees.mad.planty.di
+
+
+
+
+object HiltModule {
+
+
+
+}
